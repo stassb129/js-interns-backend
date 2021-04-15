c2cc05ed65c0bbfec12398b5d9968ba7f6b20f9d
stassb129 <45415961+stassb129@users.noreply.github.com>
2021-04-15 17:03:05 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ValidationPipe} from "@nestjs/common";
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('JS interns')
+        .setDescription('test project')
+        .setVersion('3')
+        .addTag('cats')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(4000);
+}
+
+bootstrap();
