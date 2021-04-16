2ebe6bb4bafb0afb7397abb2d52a5630425d49d5
stassb129 <45415961+stassb129@users.noreply.github.com>
2021-04-16 18:01:13 +0300

    auth

--- src/photo/photo.module.ts
+++ src/photo/photo.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database.module';
+import { DatabaseModule } from '../database/database.module';
 import { photoProviders } from './photo.providers';
 import { PhotoService } from './photo.service';
 
