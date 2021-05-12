815b8718f69acc4dd1f39c581301fa930c10af96
stassb129 <45415961+stassb129@users.noreply.github.com>
2021-05-12 17:47:03 +0300

    Update pinktada-items.service.ts

--- src/pinktada-items/pinktada-items.service.ts
+++ src/pinktada-items/pinktada-items.service.ts
@@ -13,6 +13,32 @@ export class PinktadaItemsService {
     async findAll(): Promise<Item[]> {
         return this.itemModel
             .find()
+            .limit(50)
+            .exec()
+    }
+
+    async findByIds(ids: String[]): Promise<Item[]> {
+        return this.itemModel
+            .find({
+                _id: {$in: ids}
+            }, {
+                listing: {
+                    name: 1,
+                    avgRating: 1,
+                    kickerContent: {messages: 1},
+                    pictureUrl: 1,
+                    reviews: 1,
+                    reviewsCount: 1,
+                    pricingQuote: {priceString: 1}
+                }
+            })
+            .limit(20)
+            .exec()
+    }
+
+    async findOneById(): Promise<Item[]> {
+        return this.itemModel
+            .find()
             .limit(1)
             .exec()
     }
@@ -34,4 +60,33 @@ export class PinktadaItemsService {
             .limit(500)
             .exec();
     }
+
+    async findPlacesByBoxChords(coordsItemsDto): Promise<Item[]> {
+        return this.itemModel
+            .find({
+                    location: {
+                        $geoWithin: {
+                            $box: [
+                                coordsItemsDto.leftBottomCoords.map(e => Number(e)),
+                                coordsItemsDto.rightTopCoords.map(e => Number(e))
+                            ]
+                        }
+                    }
+                }, {
+                    listing: {
+                        _id: 1,
+                        name: 1,
+                        avgRating: 1,
+                        kickerContent: {messages: 1},
+                        pictureUrl: 1,
+                        reviews: 1,
+                        reviewsCount: 1,
+                    }, pricingQuote: {priceString: 1}
+                }
+            )
+            .lean()
+            .limit(500)
+            .exec();
+    }
 }
+
